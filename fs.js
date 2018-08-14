0c224e0b7df859e772ac29018403cae484bdb50d
Ray Yang <ryang@wfsaustralia.com>
2018-08-14 17:24:35 +1000

    Test readline

--- fs.js
+++ fs.js
@@ -1,3 +1,18 @@
 const fs=require('fs');
 const os=require('os');
+const readLine=require('readline');
+let user=os.userInfo();
+let content=fs.readFileSync('./content/1.txt').toString();
+let lineCount=0;
+console.log(`Hello ${user.username}, content in the file are`,content);
 
+const rl=readLine.createInterface({
+    input:fs.createReadStream('./content/1.txt')
+})
+
+rl.on('line',function(line){
+
+    lineCount++;
+    console.log(lineCount);
+})
+console.log(lineCount);
